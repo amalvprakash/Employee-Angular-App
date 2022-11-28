81d1594fd6c80fc2dde2e186bedcf5214cbb20e0
Amal <amalprakash2025@gmail.com>
2022-11-28 23:15:07 +0530

    added binding to all forms

--- src/app/edit-employee/edit-employee.component.ts
+++ src/app/edit-employee/edit-employee.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./edit-employee.component.css']
 })
 export class EditEmployeeComponent {
+  edit = ""
 
+  Edit = () => {
+    let data:any = {
+      "edit":this.edit
+    }
+    console.log(data)
+  }
 }
--- src/app/employee-delete/employee-delete.component.ts
+++ src/app/employee-delete/employee-delete.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./employee-delete.component.css']
 })
 export class EmployeeDeleteComponent {
+  delete = ""
 
+  Delete = () => {
+    let data:any = {
+      "delete":this.delete
+    }
+    console.log(data)
+  }
 }
