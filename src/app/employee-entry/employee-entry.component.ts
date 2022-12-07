64bbaf60475fb65b2a3c8664ae117a4cb25a1665
Amal <amalprakash2025@gmail.com>
2022-12-07 14:36:20 +0530

    Addded api integration

--- src/app/employee-entry/employee-entry.component.ts
+++ src/app/employee-entry/employee-entry.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-employee-entry',
@@ -6,18 +7,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./employee-entry.component.css']
 })
 export class EmployeeEntryComponent {
+  empCode=""
   name=""
-  desg=""
+  dsgn=""
   salary=""
-  phone=""
-  
+  companyName=""
+  mobileNumber=""
+  // username=""
+  // password=""
+
+  constructor(private api:ApiService){}
   readValues =() =>{
     let data:any ={
+      "empCode":this.empCode,
       "name":this.name,
-      "desg":this.desg,
+      "dsgn":this.dsgn,
       "salary":this.salary,
-      "phone":this.phone
+      "companyName":this.companyName,
+      "mobileNumber":this.mobileNumber,
+      // "username":this.username,
+      // "password":this.password
     }
-    console.log(data)
+    this.api.addEmployee(data).subscribe(
+      (response:any) => {
+        console.log(response)
+        if (response.status == "success") {
+          alert("Patients Added SuccessFully")
+         
+         
+        } else {
+          alert("Something went wrong!!!")
+        }
+      }
+    )
   }
-}
+  }
+
--- src/app/employeelist/employeelist.component.ts
+++ src/app/employeelist/employeelist.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-employeelist',
@@ -6,65 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./employeelist.component.css']
 })
 export class EmployeelistComponent {
-  data:any = [
-    {
-      "name":"amal",
-      "desg":"TMG",
-      "salary":25000,
-      "phone":8111858402
-    },
-    {
-      "name":"bob",
-      "desg":"TMG",
-      "salary":25000,
-      "phone":8475487555
-    },
-    {
-      "name":"marly",
-      "desg":"TMG",
-      "salary":25000,
-      "phone":9333224455
-    },
-    {
-      "name":"Charlie",
-      "desg":"TMG",
-      "salary":25000,
-      "phone":9342323221
-    },
-    {
-      "name":"mary",
-      "desg":"TMG",
-      "salary":25000,
-      "phone":8456327272
-    },
-    {
-      "name":"Ram",
-      "desg":"TMG",
-      "salary":25000,
-      "phone":7389298388
-    },
-    {
-      "name":"Julie",
-      "desg":"TMG",
-      "salary":25000,
-      "phone":9897675645
-    },
-    {
-      "name":"John",
-      "desg":"TMG",
-      "salary":25000,
-      "phone":748774575
-    }
+  
+  constructor(private api:ApiService){
+    api.fetchEmployee().subscribe(
+      (response)=>{
+        this.data = response;
+      }
+    )
+  }
 
-]
-  // name = "amal"
-  // desg = "TMG"
-  // salary = 25000
-  // phone = 8111858402
-  // email = "amal@nest.com"
-  // companyName = "NEST"
-  // yearsOfExp = .03
-
-
-
+  data:any = []
+ 
 }
